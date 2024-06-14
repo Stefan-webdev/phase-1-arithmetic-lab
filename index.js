f1917c7a2b43e75741ca8086727f358fca61b61e
Stefan-webdev <154948992+Stefan-webdev@users.noreply.github.com>
2024-06-14 13:04:15 +0930

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+const multiply = (num1 * num2)
+console.log(num1 * num2)
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 44
+let num4 = 10
+const mod = num3 % num4
+
+const max = Math.max(1, 3, 4, 20)
